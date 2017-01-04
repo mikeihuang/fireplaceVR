e267c29a01c0f00e1fb3d9ac69c0ce6cb50d2873
Mikei Huang <mikei@Mikeis-MacBook-Pro.local>
2017-01-03 22:37:10 -0500

    Submitted for stupid hackathon 2016

---
+++ Assets/Script/FloorTextureTile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorTextureTile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.GetComponent<Renderer>().material.mainTextureScale = new Vector2(10 , 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
